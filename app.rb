649f9fdf4ca7bba30be40acb4ba46ddb1cb43d0e
Saud bin Mohammed <samaphp@users.noreply.github.com>
2024-08-10 02:34:59 +0300

    Use HTML templates for pages

--- app.rb
+++ app.rb
@@ -9,18 +9,19 @@ require 'yaml'
 class OfficeApp
   SETTINGS = YAML.load_file('settings.yml')
   WEBHOOK_URL = SETTINGS['webhook_url']
+  VIEW_PATH = File.join(__dir__, 'views')
 
   def call(env)
     request = Rack::Request.new(env)
 
     case request.path
     when '/coffee'
-      [200, { 'content-type' => 'text/plain' }, ['Good people']]
-    when '/ready'
+      serve_html('coffee/index.html')
+    when '/coffee/ready'
       post_to_google_chat("Ready message triggered! (via Ruby script)")
-      [200, { 'content-type' => 'text/plain' }, ['Good people']]
+      serve_html('coffee/ready.html')
     else
-      [404, { 'content-type' => 'text/plain' }, ['Not Found']]
+      serve_html('404.html', 404)
     end
   end
 
@@ -28,7 +29,7 @@ class OfficeApp
 
   def post_to_google_chat(message)
     uri = URI.parse(WEBHOOK_URL)
-    header = {'Content-Type': 'application/json'}
+    header = { 'content-type': 'application/json' }
     body = { text: message }.to_json
 
     http = Net::HTTP.new(uri.host, uri.port)
@@ -38,5 +39,15 @@ class OfficeApp
 
     response = http.request(request)
     #puts "Response from Google Chat: #{response.body}"
+  end
+
+  def serve_html(filename, status = 200)
+    file_path = File.join(VIEW_PATH, filename)
+    if File.exist?(file_path)
+      content = File.read(file_path)
+      [status, { 'content-type' => 'text/html' }, [content]]
+    else
+      [404, { 'content-type' => 'text/plain' }, ['File Not Found']]
+    end
   end
 end
